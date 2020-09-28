5b07bfe3c848ace24d7a7386380de4fe90fe8c0f
Antony Simiyu <antonymunyasia993@gmail.com>
2020-09-28 14:25:08 +0300

    Added quotes array and class Quotes

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,14 @@
 export class Quote {
-}
+    public showDescription: boolean;
+    constructor(
+      public id: number,
+      public description: string,
+      public name: string,
+      public publisher: string,
+      public upVote: number,
+      public downVote: number, 
+      public publishDate: Date,) {
+      this.showDescription = false
+    }
+  }
+  
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote',
@@ -6,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+
+  qoutes:Quote[] = [
+    new Quote (1, 'Antony', 'Worry Not', '“There is only one way to happiness and that is to cease worrying about things which are beyond the power of our will.” — Epictetus', 0, 0, new Date(2020,8,27) ),
+    new Quote(2, 'James ', 'Happiness','“Happiness is not something ready-made. It comes from your own actions.”— Dalai Lama', 0, 0, new Date(2020,8,27)),
+    new Quote (3, 'Thomas', 'Men and Wealth','“For many men, the acquisition of wealth does not end their troubles, it only changes them.”— Seneca', 0, 0, new Date(2020,8,27)),
+    new Quote (4, 'Kai', 'Harmony', '“Happiness is when what you think, what you say, and what you do are in harmony.” — Mahatma Gandhi', 0, 0, new Date(2020,8,27)),
+    new Quote (5, 'Phyl', 'Take a Risk','“Most people would rather be certain they’re miserable, than risk being happy.” — Dr. Robert Anthony', 0, 0, new Date(2020,8,27)),
+    new Quote (6, 'Jay', 'Give', '"Since you get more joy out of giving joy to others, you should put a good deal of thought into the happiness that you are able to give.”— Eleanor Roosevelt' , 0, 0, new Date(2020,8,27)),
+  ];
+
 
   constructor() { }
 
