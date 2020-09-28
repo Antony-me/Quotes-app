c992143d6692761590715687c237d0ceadcf9f8f
Antony Simiyu <antonymunyasia993@gmail.com>
2020-09-28 15:20:54 +0300

    Added bootstrap

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,7 +8,7 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
 
-  qoutes:Quote[] = [
+  quotes:Quote[] = [
     new Quote (1, 'Antony', 'Worry Not', '“There is only one way to happiness and that is to cease worrying about things which are beyond the power of our will.” — Epictetus', 0, 0, new Date(2020,8,27) ),
     new Quote(2, 'James ', 'Happiness','“Happiness is not something ready-made. It comes from your own actions.”— Dalai Lama', 0, 0, new Date(2020,8,27)),
     new Quote (3, 'Thomas', 'Men and Wealth','“For many men, the acquisition of wealth does not end their troubles, it only changes them.”— Seneca', 0, 0, new Date(2020,8,27)),
@@ -16,6 +16,24 @@ export class QuoteComponent implements OnInit {
     new Quote (5, 'Phyl', 'Take a Risk','“Most people would rather be certain they’re miserable, than risk being happy.” — Dr. Robert Anthony', 0, 0, new Date(2020,8,27)),
     new Quote (6, 'Jay', 'Give', '"Since you get more joy out of giving joy to others, you should put a good deal of thought into the happiness that you are able to give.”— Eleanor Roosevelt' , 0, 0, new Date(2020,8,27)),
   ];
+
+
+  toggleDetails(index) {
+    this.quotes[index].showDescription = !this.quotes[index].showDescription;
+  }
+
+  completeQuote(isComplete, index) {
+    if (isComplete) {
+      this.quotes.splice(index, 1);
+    }
+  }
+  addNewQuote(quote) {
+    let quoteLength = this.quotes.length;
+    quote.id = quoteLength + 1;
+    quote.publishDate = new Date(quote.publishDate)
+    this.quotes.unshift(quote)
+
+  }
 
 
   constructor() { }
